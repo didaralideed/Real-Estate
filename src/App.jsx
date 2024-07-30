71f62d1f733475ab726d5df348fb6b1b51d050f1
Didar  ALi <163466513+didaralideed1@users.noreply.github.com>
2024-07-30 22:10:31 +0500

    Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import { BrowserRouter, Routes,Route } from "react-router-dom"
-import SignUp from "./pages/SignUp"
-import Sigin from "./pages/SignIn"
-import About from "./pages/About"
-import Profile from "./pages/Profile"
-import SignIn from "./pages/SignIn"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 
 export default function App() {
-  return <BrowserRouter>
-  <div>Home</div>
-  <Routes>
-    <Route path="/" element={<Home/>}/>
-    <Route path="/gign-up" element={<SignUp/>}/>
-   <Route path="/sign-in" element={<SignIn/>}/>
-    <Route path="/about" element={<About/>}/>
-    <Route path="/profile" element={<Profile/>}/>
-  </Routes>
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
